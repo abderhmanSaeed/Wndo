b1a96719b5de35801200c8339b5ce0dee7af0bc8
heba-elsafty <heba.m.elsafty@gmail.com>
2024-01-04 02:31:49 +0200

    Remove extra icon compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { NavComponent } from './layout/nav/nav.component';
-import { SharedModule } from "./shared/shared.module";
-
 
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { IconComponent } from './shared/components/icon/icon.component';
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        AuthLayoutComponent,
-        ContentLayoutComponent,
-        FooterComponent,
-        NavComponent,
-        IconComponent
-    ],
-    exports: [IconComponent],
-    providers: [
-        provideClientHydration()
-    ],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule
-    ],
+  declarations: [
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent,
+    FooterComponent,
+    NavComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

150176773e648ed56c83894362d9ea7cba7eb204
asaeed.vpn <asaeed.vpn@nupco.com>
2024-01-07 18:39:08 +0200

    merge

--- src/app/modules/product/order-process/order-process.component.ts
+++ src/app/modules/product/order-process/order-process.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
+  standalone: true,
+  imports: [SharedModule],
   selector: 'app-order-process',
   templateUrl: './order-process.component.html',
   styleUrl: './order-process.component.scss',
-  standalone: true,
-  imports: [SharedModule],
 })
 export class OrderProcessComponent {
 

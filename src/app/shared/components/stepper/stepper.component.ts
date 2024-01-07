b8a8cf9160fecb471b3aaf83f78ad0ea8d8bedb1
heba-elsafty <heba.m.elsafty@gmail.com>
2024-01-07 21:30:24 +0200

    Enhance Stepper

--- src/app/shared/components/stepper/stepper.component.ts
+++ src/app/shared/components/stepper/stepper.component.ts
@@ -1,32 +1,38 @@
-import { NgFor, NgClass, NgIf, NgTemplateOutlet } from "@angular/common";
-import { TemplateRef, Component, AfterContentInit, Output, EventEmitter, ContentChildren, QueryList, Input } from "@angular/core";
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+  Input,
+  Renderer2,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { StepComponent } from './step/step.component';
 
-import { StepComponent } from "./step/step.component";
-
-
-interface Step {
-  label: string;
-  icon: string;
-  template: TemplateRef<any> | null;
-  nextButtonLabel?: string;
-  additionalAction?: any;
-}
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.scss'
+  styleUrls: ['./stepper.component.scss'],
 })
 export class StepperComponent implements AfterContentInit {
-  @Input() btnLabel:string = 'Next';
-  @Input() disabledNextButton:boolean =  false;
+  @Input() btnLabel: string = 'Next';
+  @Input() disabledNextButton: boolean = false;
+
+  constructor(private renderer: Renderer2) {}
 
   @ContentChildren(StepComponent) steps: QueryList<StepComponent> | undefined;
   currentStep: number = 0;
-  currentStepTemplate: TemplateRef<any> | null = null;
+  currentStepTemplate: any = null;
 
-  ngAfterContentInit(): void {
-    // Ensure steps are initialized before updating the template
+  @ViewChild('stepContainer') stepsElement: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    this.calculateLineWidths();
+  }
+
+  ngAfterContentInit() {
     this.steps?.changes.subscribe(() => {
       this.updateCurrentStepTemplate();
     });
@@ -40,7 +46,7 @@ export class StepperComponent implements AfterContentInit {
   }
 
   backStep() {
-    if (this.steps && this.currentStep < this.steps.length - 1) {
+    if (this.steps && this.currentStep > 0) {
       this.currentStep--;
       this.updateCurrentStepTemplate();
     }
@@ -48,18 +54,43 @@ export class StepperComponent implements AfterContentInit {
 
   private updateCurrentStepTemplate() {
     if (this.steps) {
-      this.currentStepTemplate = this.steps.toArray()[this.currentStep]?.template || null;
+      this.currentStepTemplate =
+        this.steps.toArray()[this.currentStep]?.template || null;
     }
   }
 
-  private updateStepActivation() {
-    if (this.steps) {
-      this.steps.toArray().forEach((step, index) => {
-        step.isActive = index === this.currentStep;
+
+  calculateLineWidths() {
+    let stepElementsArray: any[] = [];
+    if (this.steps && this.stepsElement) {
+      stepElementsArray = this.stepsElement.nativeElement.childNodes;
+
+      stepElementsArray.forEach((step: any, index: number) => {
+        if (index < stepElementsArray.length - 1) {
+          const nextStep = stepElementsArray[index + 1];
+          const line = step.children[0].children[0];
+
+          let lineWidth: number;
+          const lengthOfSteps = (this.steps && this.steps.length) || 2
+          if (index === 0) {
+            // For the first step, adjust the calculation
+            lineWidth =
+              nextStep.offsetLeft +
+              nextStep.clientWidth / lengthOfSteps - 1 -
+              step.offsetLeft -
+              step.clientWidth / lengthOfSteps - 1;
+          } else {
+            // For subsequent steps
+            lineWidth =
+              nextStep.offsetLeft -
+              step.offsetLeft +
+              nextStep.clientWidth / lengthOfSteps -
+              step.clientWidth / 2.5 ;
+          }
+
+          this.renderer.setStyle(line, 'width', `${lineWidth}px`);
+        }
       });
     }
   }
-
-
-
 }

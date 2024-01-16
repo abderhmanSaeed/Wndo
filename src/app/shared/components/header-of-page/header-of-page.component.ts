702e48fff1567f47a1db4bf063d0b5202ba1c5e3
asaeed.vpn <asaeed.vpn@nupco.com>
2024-01-16 09:40:51 +0200

    routing productOffers edit

--- src/app/shared/components/header-of-page/header-of-page.component.ts
+++ src/app/shared/components/header-of-page/header-of-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-header-of-page',
@@ -9,24 +9,20 @@ export class HeaderOfPageComponent implements OnInit {
   @Input() seller!: any;
   isProductOffersRoute: boolean = false;
 
-  constructor(private renderer: Renderer2, private el: ElementRef, private route: ActivatedRoute, private router: Router,) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe(paramMap => {
-      this.isProductOffersRoute = paramMap.get('sellerId') !== null;
+    this.route.queryParams.subscribe(queryParams => {
+      this.isProductOffersRoute = 'sellerId' in queryParams;
     });
   }
 
   logSellerId() {
     let sellerId = this.seller?.id;
-    if (!sellerId) {
-      sellerId = '3e6baab3-d304-435c-63f9-3a0427ec59c8';
-
-    }
     // Only navigate if not on the productOffers route
     if (!this.isProductOffersRoute) {
       // Navigate to the product/productOffers route with seller ID
-      this.router.navigate(['/product/productOffers', { sellerId: sellerId }]);
-    }
+      this.router.navigate(['/product/productOffers', { sellerId: sellerId }]);    }
   }
 
 

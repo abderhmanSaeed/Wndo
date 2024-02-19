0a3bbe89ef0f55f1735a41baea03d3b42d7fb534
asaeed.vpn <asaeed.vpn@nupco.com>
2024-02-19 18:02:37 +0200

    handle shar product info

--- src/app/shared/components/product/product-snapshot-card/product-snapshot-card.component.ts
+++ src/app/shared/components/product/product-snapshot-card/product-snapshot-card.component.ts
@@ -11,39 +11,48 @@ import { ModalDataService } from '../../modal/modal.data.service';
 export class ProductSnapshotCardComponent {
   @Input() product: any;
 
-   // Variable to control the visibility of the copied message
-   showCopiedMessage: boolean = false;
+  // Variable to control the visibility of the copied message
+  showCopiedMessage: boolean = false;
 
-   constructor(private modalService: ModalService , private modalDataService: ModalDataService){}
+  constructor(private modalService: ModalService, private modalDataService: ModalDataService) { }
 
-   // Method to copy the share URL to the clipboard and show the message
-   shareProduct() {
-     // Create a temporary textarea element
-     const textarea = document.createElement('textarea');
-     textarea.value = this.product.shareUrl;
+  // Method to copy the share URL and additional product information to the clipboard and show the message
+  shareProduct() {
+    // Create a temporary textarea element
+    const textarea = document.createElement('textarea');
 
-     // Append the textarea to the document
-     document.body.appendChild(textarea);
+    // Concatenate product information into a formatted string
+    const productInfo = `URL: ${this.product.shareUrl}\n` +
+      `Name: ${this.product?.name || 'N/A'}\n` +
+      `Description: ${this.product?.description || 'N/A'}\n` +
+      `Image URL: ${this.product?.image?.urlPreview || 'N/A'}`;
 
-     // Select the text inside the textarea
-     textarea.select();
+    // Set the value of the textarea to the concatenated product information
+    textarea.value = productInfo;
 
-     // Copy the selected text to the clipboard
-     document.execCommand('copy');
+    // Append the textarea to the document
+    document.body.appendChild(textarea);
 
-     // Remove the textarea from the document
-     document.body.removeChild(textarea);
+    // Select the text inside the textarea
+    textarea.select();
 
-     // Show the copied message
-     this.showCopiedMessage = true;
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
 
-     // Hide the message after a delay (e.g., 3 seconds)
-     setTimeout(() => {
-       this.showCopiedMessage = false;
-     }, 3000);
-   }
+    // Remove the textarea from the document
+    document.body.removeChild(textarea);
 
-   openAddProductToCardModal(product: any) {
+    // Show the copied message
+    this.showCopiedMessage = true;
+
+    // Hide the message after a delay (e.g., 3 seconds)
+    setTimeout(() => {
+      this.showCopiedMessage = false;
+    }, 3000);
+  }
+
+
+  openAddProductToCardModal(product: any) {
     this.modalDataService.setData({ product: product });
     this.modalService.open(AddProductToCardModalComponent, {
       animations: {
